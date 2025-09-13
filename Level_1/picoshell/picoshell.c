45f099863b8b3adf73ca2f8e7d82ce65d5f2e901
Mohamed el haia <127893038+MR-SIMONN@users.noreply.github.com>
2025-09-13 21:49:53 +0100

    validated with ✅

--- Level_1/picoshell/picoshell.c
+++ Level_1/picoshell/picoshell.c
@@ -2,74 +2,52 @@
 
 int picoshell(char **cmds[])
 {
+    int i = 0;
+    int fd[2];
+    int in_fd = 0;
+    pid_t pid;
+
     if (!cmds)
         return 1;
-    int prev_read_fd = -1;
-    int pipe_fd[2];
-    int pid;
-    int i = 0;
-    int children = 0;
-
-    pipe_fd[0] = -1;
-    pipe_fd[1] = -1;
     while (cmds[i])
     {
-        if (cmds[i + 1] && pipe(pipe_fd) == -1)
+        if (cmds[i + 1])
+            pipe(fd);
+        else
         {
-            if (prev_read_fd != -1)
-                close (prev_read_fd);
-            return 1;
+            fd[0] = -1;
+            fd[1] = -1;
         }
 
         pid = fork();
-        if (pid == -1)
-        {
-            if (prev_read_fd != -1)
-                close (prev_read_fd);
-            if (cmds[i + 1])
-            {
-                close (pipe_fd[0]);
-                close (pipe_fd[1]);
-            }
-            return 1;
-        }
-
         if (pid == 0)
         {
-            if (prev_read_fd != -1)
+            if (in_fd != 0)
             {
-                dup2(prev_read_fd, STDIN_FILENO);
-                close(prev_read_fd);
+                dup2(in_fd, STDIN_FILENO);
+                close(in_fd);
             }
-
-            if (cmds[i + 1])
+            if (fd[1] != -1)
             {
-                dup2(pipe_fd[1], STDOUT_FILENO);
-                close(pipe_fd[1]);
-                close(pipe_fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+                close(fd[0]);
             }
-
             execvp(cmds[i][0], cmds[i]);
-            exit(1);
+            exit(0);
         }
-
-        children++;
-
-        if (prev_read_fd != -1)
-            close(prev_read_fd);
-        if (cmds[i + 1])
+        else
         {
-            close(pipe_fd[1]);
-            prev_read_fd = pipe_fd[0];
+            if (in_fd != 0)
+                close(in_fd);
+            if (fd[1] != -1)
+                close(fd[1]);
+            in_fd = fd[0];
+            i++;
         }
-        i++;
     }
 
-    for (int j = 0; j < children; j++)
-    {
-        if (wait(NULL) == -1)
-            return 1;
-    }
-
+    while (wait(NULL) > 0)
+         ;
     return 0;
 }
