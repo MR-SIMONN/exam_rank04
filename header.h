6e7280058e2c681e558a0c4c6719e723cc6dd6fd
Mohamed el haia <127893038+MR-SIMONN@users.noreply.github.com>
2025-09-18 11:54:56 +0100

    added wait lib

--- header.h
+++ header.h
@@ -2,14 +2,11 @@
 # define HEADER_H
 
 #include <string.h>
-#include <libc.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
 #include <ctype.h>
+#include <wait.h>
 
 char    *get_next_line(int fd);
 
